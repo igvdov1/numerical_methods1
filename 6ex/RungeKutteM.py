7b0e3abb1197d0c85975e18a9a727a3d327d7d9a
igvdov1 <i.vdovin1@g.nsu.ru>
2022-11-13 13:14:07 +0700

    temp

--- 6ex/RungeKutteM.py
+++ 6ex/RungeKutteM.py
@@ -1,8 +1,10 @@
 import numpy as np
+
+
 def RungeKutteMethod(f, start, fstart, x2, eps, orig):
-    h = 11
+    h = 200
+    
     k = f(start, fstart)
-    a = []
     mas = np.linspace(start, x2, h)
     h1 = (x2-start)/(h-1)
     k1 = f(start, fstart)
@@ -12,8 +14,15 @@ def RungeKutteMethod(f, start, fstart, x2, eps, orig):
     dy = h1/6*(k1 + 2 * k2 + 2 * k3 + k4)
     y1 = fstart + dy
     ans = []
-    # ans.append(start, fstart, y1, k1, k2, k3, k4)
-    
+    ans.append([start, fstart, k1, k2, k3, k4, y1])
+    for i in range(1, 200):
+        k1 = f(mas[i], ans[i-1][6])
+        k2 = f(mas[i] + h1/2, ans[i-1][6] + h1/2 * k1)
+        k3 = f(mas[i] + h1/2, ans[i-1][6] + h1/2 * k2)
+        k4 = f(mas[i] + h1, ans[i-1][6] + h1 * k3)
+        dy = h1/6 * (k1 + 2 * k2 + 2 * k3 + k4)
+        y1 = ans[i-1][6] + dy
         
-    print(start+h1/2, fstart + h1/2*k1)
-    return k1, k2, k3, k4
+        ans.append([mas[i], ans[i-1][6], k1, k2, k3, k4, y1])
+    
+    return ans
--- 6ex/test.py
+++ 6ex/test.py
@@ -1,10 +1,37 @@
 from RungeKutteM import RungeKutteMethod
 from math import exp
+import math
+import pandas as pd
+from matplotlib import pyplot as plt
+import numpy as np
+def some_tests(x, y): 
+    return x ** 2 - 2 * y
 def f(x, y): 
-    return x**2 - 2 * y
-def answer(x, y):
-      return 3/4*exp(-2*x) + 1/2 * (x**2) - 1/2 * x+ 1/4
+    return 2 * x - 5 * y + 3
+def f1(x, y):
+    return 5 * x - 6 * y + 1
+def answerx(t):
+      return 5 * exp(-2 * t) * math.cos(3 * t) + 1
+def answery(t):
+    return exp(-2 * t) * (4 * math.cos(3 * t) + 3 * math.sin(3 * t)) + 1
 if __name__ == '__main__':
     start = 1
     eps = 1
-    print(RungeKutteMethod(f, 0, 1, 1, eps, answer))
+    print(pd.DataFrame(RungeKutteMethod(some_tests, 0, 1, 1, eps, answerx)))
+    c = []
+    b = []
+    k = RungeKutteMethod(f, 0, 6, 1, eps, answerx)
+    a = np.linspace(0, 1, len(k))
+    print(answerx(0))
+    for i in range (len(k)):
+        b.append(answerx(a[i]))
+        c.append(k[i][1])
+    
+        
+        
+    plt.plot(a, c, color = 'black')
+    plt.plot(a, b, color = 'blue')
+    plt.scatter(a, c, color = 'green')
+    plt.scatter(a, b, color = 'red')
+    plt.show()
+    
