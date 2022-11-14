5242bb5072d09134ed02b3bf33514c5afc7f054a
igvdov1 <i.vdovin1@g.nsu.ru>
2022-11-14 15:36:25 +0700

    sth

--- 6ex/RungeKutteM.py
+++ 6ex/RungeKutteM.py
@@ -1,28 +1,61 @@
 import numpy as np
 
-
-def RungeKutteMethod(f, start, fstart, x2, eps, orig):
-    h = 200
-    
-    k = f(start, fstart)
-    mas = np.linspace(start, x2, h)
-    h1 = (x2-start)/(h-1)
-    k1 = f(start, fstart)
-    k2 = f(start+h1/2, fstart + h1/2*k1)
-    k3 = f(start + h1/2, fstart + h1/2*k2)
-    k4 = f(start + h1, fstart + h1 * k3)
-    dy = h1/6*(k1 + 2 * k2 + 2 * k3 + k4)
-    y1 = fstart + dy
-    ans = []
-    ans.append([start, fstart, k1, k2, k3, k4, y1])
-    for i in range(1, 200):
-        k1 = f(mas[i], ans[i-1][6])
-        k2 = f(mas[i] + h1/2, ans[i-1][6] + h1/2 * k1)
-        k3 = f(mas[i] + h1/2, ans[i-1][6] + h1/2 * k2)
-        k4 = f(mas[i] + h1, ans[i-1][6] + h1 * k3)
-        dy = h1/6 * (k1 + 2 * k2 + 2 * k3 + k4)
-        y1 = ans[i-1][6] + dy
+# def Runge_for_h(f, start, fstart, x2, h):
+#     k = f(start, fstart)
+#     mas = np.linspace(start, x2, h)
+#     h1 = (x2-start)/(h-1)
+#     k1 = f(start, fstart)
+#     k2 = f(start+h1/2, fstart + h1/2*k1)
+#     k3 = f(start + h1/2, fstart + h1/2*k2)
+#     k4 = f(start + h1, fstart + h1 * k3)
+#     dy = h1/6*(k1 + 2 * k2 + 2 * k3 + k4)
+#     y1 = fstart + dy
+#     ans = []
+#     ans.append([start, fstart, k1, k2, k3, k4, y1])
+#     for i in range(1, h):
+#         k1 = f(mas[i], ans[i-1][6])
+#         k2 = f(mas[i] + h1/2, ans[i-1][6] + h1/2 * k1)
+#         k3 = f(mas[i] + h1/2, ans[i-1][6] + h1/2 * k2)
+#         k4 = f(mas[i] + h1, ans[i-1][6] + h1 * k3)
+#         dy = h1/6 * (k1 + 2 * k2 + 2 * k3 + k4)
+#         y1 = ans[i-1][6] + dy
         
-        ans.append([mas[i], ans[i-1][6], k1, k2, k3, k4, y1])
+#         ans.append([mas[i], ans[i-1][6], k1, k2, k3, k4, y1])
     
-    return ans
+#     return ans
+
+
+def RungeKutteMethod(f, g, start, xstart, ystart, t2, eps, orig, orig2):
+    h = 2
+    a = []
+    j = 0
+    while True:
+        ans = []
+    
+        mas = np.linspace(start, t2, h)
+        ans.append([xstart, ystart, h])
+        h1 = (t2 - start) / (h-1) 
+
+        for i in range(1, h):
+            k1 = f(mas[i], ans[i - 1][0], ans[i - 1][1]) 
+            m1 = g(mas[i], ans[i - 1][0], ans[i - 1][1]) 
+            k2 = f(mas[i] + h1 / 2, ans[i - 1][0] + h1 *k1 / 2, ans[i -1 ][1] + h1 * m1 / 2) 
+            m2 = g(mas[i] + h1 / 2, ans[i - 1][0] + h1 *k1 / 2, ans[i - 1][1] + h1 * m1 / 2) 
+            k3 = f(mas[i] + h1 / 2, ans[i - 1][0] + h1 * k2 / 2, ans[i - 1][1] + h1 * m2 / 2) 
+            m3 = g(mas[i] + h1 / 2, ans[i - 1][0] + h1 * k2 / 2, ans[i - 1][1] + h1 * m2 / 2) 
+            k4 = f(mas[i] + h1, ans[i -1][0] + h1 * k3, ans[i - 1][1] + h1 * m3) 
+            m4 = g(mas[i] + h1, ans[i - 1][0] + h1 * k3, ans[i - 1][1] + h1 * m3) 
+            dx = h1/6 * (k1 + 2 * k2 + 2 * k3 + k4)
+            dy = h1/6 * (m1 + 2 * m2 + 2 * m3 + m4)
+            ans.append([ans[i - 1][0] + dx, ans[i - 1][1] + dy, h])
+        
+    
+        if(h != 2):
+            print(i)
+            a.append([ans[h - 1][0], ans[h - 1][1], ans[h - 1][0] / a[j - 1][0], ans[h - 1][1] / a[j - 1][1],  abs(ans[h - 1][0] - orig(mas[h - 1])), abs(ans[h - 1][1] - orig2(mas[h - 1])), abs(ans[h - 1][0] - orig(mas[h - 1]))/(a[j - 1][5]), abs(ans[h - 1][1] - orig2(mas[h - 1]))/(a[j - 1][5])])
+        else:
+            a.append([ans[h - 1][0], ans[h - 1][1], 'null', 'null',  abs(ans[h - 1][0] - orig(mas[h - 1])), abs(ans[h - 1][1] - orig2(mas[h - 1])), 'null', 'null'])
+        if abs(ans[h - 1][0] - orig(mas[h - 1])) < eps and abs(ans[h - 1][1] - orig2(mas[h - 1])) < eps:
+            return {'ans': ans, 'a': a}
+        j += 1    
+        h *= 2
--- 6ex/test.py
+++ 6ex/test.py
@@ -1,14 +1,15 @@
+
 from RungeKutteM import RungeKutteMethod
 from math import exp
 import math
 import pandas as pd
 from matplotlib import pyplot as plt
 import numpy as np
-def some_tests(x, y): 
-    return x ** 2 - 2 * y
-def f(x, y): 
+from CaushiM import eilerMethod
+from tabulate import tabulate
+def xfunc(t, x, y): 
     return 2 * x - 5 * y + 3
-def f1(x, y):
+def yfunc(t, x, y):
     return 5 * x - 6 * y + 1
 def answerx(t):
       return 5 * exp(-2 * t) * math.cos(3 * t) + 1
@@ -16,22 +17,37 @@ def answery(t):
     return exp(-2 * t) * (4 * math.cos(3 * t) + 3 * math.sin(3 * t)) + 1
 if __name__ == '__main__':
     start = 1
-    eps = 1
-    print(pd.DataFrame(RungeKutteMethod(some_tests, 0, 1, 1, eps, answerx)))
-    c = []
-    b = []
-    k = RungeKutteMethod(f, 0, 6, 1, eps, answerx)
-    a = np.linspace(0, 1, len(k))
-    print(answerx(0))
-    for i in range (len(k)):
-        b.append(answerx(a[i]))
-        c.append(k[i][1])
+    eps = 1e-12
+    xstart = 6
+    ystart = 5
+    d = []
+    head = ['N', 'eps', 'y', 'yi/y(i-1)']
+    # while eps >= 1e-12:
+    #     k = RungeKutteMethod(xfunc, yfunc, 0, xstart, ystart, 10, eps, answerx, answery)['ans']
+    #     eps /= 10
+    #     d.append([len(k), eps, k[len(k)-1][1], k[len(k)-1][1]/answerx(10)])
+    # print(tabulate(d, headers = head, tablefmt = 'grid'))
+    k = RungeKutteMethod(xfunc, yfunc, 0, xstart, ystart, 1, eps, answerx, answery)['ans']
+    print(pd.DataFrame(k))
+    b = RungeKutteMethod(xfunc, yfunc, 0, xstart, ystart, 1, eps, answerx, answery)['a']
+    print(pd.DataFrame(b))
+    a = np.linspace(0, 1, k[len(k) - 1][2])
     
-        
-        
-    plt.plot(a, c, color = 'black')
-    plt.plot(a, b, color = 'blue')
-    plt.scatter(a, c, color = 'green')
-    plt.scatter(a, b, color = 'red')
+
+    my = []
+    my1 = []
+    orig = []
+    orig1 = []
+    for i in range(len(k)): 
+        my.append(k[i][1])
+        my1.append(k[i][0])
+        orig.append(answery(a[i]))
+        orig1.append(answerx(a[i]))
+    plt.plot(a,orig1 , color = 'blue')
+    plt.plot(a, orig, color = "red")
+    plt.plot(a, my, color = 'orange')
+    plt.plot(a, my1, color = 'green')
+    plt.scatter(a, orig1, color = "green")
+    plt.scatter(a, orig, color = 'black')
     plt.show()
-    
+ 
